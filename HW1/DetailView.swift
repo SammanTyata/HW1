66d19180dfbf137cfc946b93df9e17268aa9bf45
Samman <tyatasamman@gmail.com>
2024-10-05 21:26:57 -0700

    ++ HW1 requirements

---
+++ HW1/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  HW1
+//
+//  Created by Samman Tyata on 10/5/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
