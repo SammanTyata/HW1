ba8b03acec98c9487be60d346c66d2cca7aa8310
Samman <tyatasamman@gmail.com>
2024-10-05 21:12:43 -0700

    Initial Commit

---
+++ HW1/HW1App.swift
@@ -0,0 +1,17 @@
+//
+//  HW1App.swift
+//  HW1
+//
+//  Created by Samman Tyata on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct HW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
